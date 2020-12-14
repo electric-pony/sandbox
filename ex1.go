1e10bf0d5449e1122a7071fd70432d249c6577ec
Roslynn Ricard <roslynnricard@gmail.com>
2020-12-13 22:36:25 -0800

    exercise 1

--- ex1.go
+++ ex1.go
@@ -5,5 +5,23 @@ import (
 )
 
 func main() {
+	var a int
+	var b int = 10
+	var c = 10
+	var d, e, f bool
+
+	var (
+		g       int
+		h       string
+		i       int = 1234
+		j, k, l bool
+	)
+
+	m := 1
+	n, o := 2, 3
+	a = 11
+	e
+
 	fmt.Println("Hello, world!")
+	fmt.Println(a)
 }
